c3ac461468a3d4d4ea004d404ee947aac62e853d
Sachin Shetty <sachin.shetty@marksandspencer.com>
2021-10-19 01:57:02 +0100

    debugging entrypoint

--- newrelic-get-appid/entrypoint.sh
+++ newrelic-get-appid/entrypoint.sh
@@ -2,7 +2,8 @@
 
 app-name = $1
 api-key = $2
-echo $2 $1
+echo $1
+echo $2
 response=$(curl -X GET 'https://api.newrelic.com/v2/applications.json' \
                      -H 'Content-Type: application/json' \
                      -H 'X-Api-Key:'$2 \
