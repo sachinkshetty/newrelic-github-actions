7237e8f46bdca8237c855985b72d20a8c495bfa5
Sachin Shetty <sachin.shetty@marksandspencer.com>
2021-10-19 01:53:20 +0100

    debugging entrypoint

--- newrelic-get-appid/entrypoint.sh
+++ newrelic-get-appid/entrypoint.sh
@@ -7,6 +7,6 @@ response=$(curl -X GET 'https://api.newrelic.com/v2/applications.json' \
                      -H 'Content-Type: application/json' \
                      -H 'X-Api-Key:'$2 \
                      -G -d 'filter[name]='$1 )
-
+echo $response
 nr_app_id=$(jq ".applications[].id" <<< "$response")
 echo "::set-output name=nr_app_id::$nr_app_id"
