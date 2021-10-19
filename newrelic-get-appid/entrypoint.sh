30fb1e9fbac67ca8a2fad811d6e72fdfb0752da8
Sachin Shetty <sachin.shetty@marksandspencer.com>
2021-10-19 01:47:32 +0100

    changing docker file and entrypoint

--- newrelic-get-appid/entrypoint.sh
+++ newrelic-get-appid/entrypoint.sh
@@ -2,11 +2,11 @@
 
 app-name = $1
 api-key = $2
-echo $app-name
+echo $appName
 response=$(curl -X GET 'https://api.newrelic.com/v2/applications.json' \
                      -H 'Content-Type: application/json' \
-                     -H 'X-Api-Key:$api-key' \
-                     -G -d 'filter[name]=$app-name' )
+                     -H 'X-Api-Key:'$2 \
+                     -G -d 'filter[name]='$1 )
 
 nr_app_id=$(jq ".applications[].id" <<< "$response")
-echo "::set-output name=nr_app_id::$nr_app_id" 
+echo "::set-output name=nr_app_id::$nr_app_id"
